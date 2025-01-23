605e389d358ec235cc480c7352bfcb75a43ff467
Aomsub <lerdariyamaythee@gmail.com>
2025-01-23 10:12:31 +0700

    Finalize plot_work

--- plot.py
+++ plot.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 
 x = np.linspace(-5, 5, 40)
 x_square = x ** 2
-plt.plot(x, x_square, label='x_square', color='green', marker='*')
+plt.plot(x, x_square, label='f(x) = x^2', color='green', marker='*')
 
 x_t_sin2x = x * np.sin(2*x)
-plt.plot(x, x_t_sin2x, label='x * sin(2*x)', color='red', linestyle='--', marker='1')
+plt.plot(x, x_t_sin2x, label='f(x) = x * sin(2x)', color='red', linestyle='--', marker='1')
 
 arctg = np.arctan(x)
-plt.plot(x, arctg, label='arctg', color='blue', linestyle=':', marker='4')
+plt.plot(x, arctg, label='f(x) = arctan(x)', color='blue', linestyle=':', marker='4')
 
 plt.xlabel('x')
 plt.ylabel('y')
