e73494f67bd1318b29fc7ae743fbf44feebbbc6a
Aomsub <lerdariyamaythee@gmail.com>
2025-01-23 09:55:52 +0700

    Plot x square

--- plot.py
+++ plot.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+x = np.linspace(-5, 5, 200)
+x_square = x ** 2
+plt.plot(x, x_square, label='x_square', color='green')
 
-def x_square_plot():
-    pass
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('All plots')
+plt.legend()
+plt.grid(True)
+plt.show()
 
-def x_t_sin2x_plot():
-    pass
-
-def arctg_plot():
-    pass
