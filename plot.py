d5e26f9de4570a6f8c973bbfc17676477f774362
Aomsub <lerdariyamaythee@gmail.com>
2025-01-23 09:56:33 +0700

    Plot sin2x graph

--- plot.py
+++ plot.py
@@ -5,6 +5,9 @@ x = np.linspace(-5, 5, 200)
 x_square = x ** 2
 plt.plot(x, x_square, label='x_square', color='green')
 
+x_t_sin2x = x * np.sin(2*x)
+plt.plot(x, x_t_sin2x, label='x * sin(2*x)', color='red')
+
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('All plots')
